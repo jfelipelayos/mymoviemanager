f5d5126a0e72baaee6897f9b50e0bd68bbe3d4f1
Felipe Layos <jfelipelayos@gmail.com>
2021-10-22 17:45:14 -0500

    Fixed header logo and favicon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ function Header() {
         <header className="header">
             <Link to="/">
                 <div className="header-left-info">
-                    <img className="header-logo" src="https://firebasestorage.googleapis.com/v0/b/globant-bootcamp-project.appspot.com/o/Filmlerin%20(1).png?alt=media&token=687dc84d-a27b-4ced-ac7a-950b5aee66af" alt="Logo de My Movie Manager" />
+                    <img className="header-logo" src="https://firebasestorage.googleapis.com/v0/b/mymoviemanager-jfelipelayos.appspot.com/o/Logo.png?alt=media&token=f0b343c0-6174-4c2b-a396-1a5913709846" alt="Logo de My Movie Manager" />
                     <h1 className='header-title'>MyMovieManager</h1>
                 </div>
             </Link>
